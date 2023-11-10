58a20deada42abb5e670c32aa749954ea74c5d14
= <=>
2023-11-10 16:16:37 +0530

    test case 3 and 5 added

--- test_sample3.py
+++ test_sample3.py
@@ -9,7 +9,7 @@ def test_tutorials_ninja():
     driver.maximize_window()
     driver.get("https://tutorialsninja.com/demo/")
     time.sleep(3)
-    expected_title="Your Store AAA"
+    expected_title="Your Store"
     actual_title=driver.title
     time.sleep(3)
     assert expected_title.__eq__(actual_title)
