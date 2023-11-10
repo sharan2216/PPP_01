7cde9ca5e70dc8738b305b62817bc753bdadb495
= <=>
2023-11-10 15:53:05 +0530

    test case 3 got updated

---
+++ test_sample3.py
@@ -0,0 +1,17 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_tutorials_ninja():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://tutorialsninja.com/demo/")
+    time.sleep(3)
+    expected_title="Your Store AAA"
+    actual_title=driver.title
+    time.sleep(3)
+    assert expected_title.__eq__(actual_title)
+    time.sleep(3)
+    driver.quit()
